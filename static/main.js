629dcc283ed4865b04d0d4a048cf830586312108
Yasser <myasser.z209@gmail.com>
2021-06-06 22:34:46 +0200

    Working Whiteboard

--- static/main.js
+++ static/main.js
@@ -1,87 +1,128 @@
 $(function () {
-    var canvas = $('.whiteboard')[0];
-    var context = canvas.getContext('2d');
-    var current = {
-        color: 'black'
+  var canvas = $(".whiteboard")[0];
+  var syncClient;
+  var syncStream;
+  var $message = $("#message");
+  var context = canvas.getContext("2d");
+  var current = {
+    color: "black",
+  };
+  var drawing = false;
+
+  function drawLine(x0, y0, x1, y1, color, syncStream) {
+    context.beginPath();
+    context.moveTo(x0, y0);
+    context.lineTo(x1, y1);
+    context.strokeStyle = color;
+    context.lineWidth = 2;
+    context.stroke();
+    context.closePath();
+
+    if (syncStream) {
+      var w = canvas.width;
+      var h = canvas.height;
+
+      syncStream.publishMessage({
+        // publish the drawing data to Twilio Sync server
+        x0: x0 / w,
+        y0: y0 / h,
+        x1: x1 / w,
+        y1: y1 / h,
+        color: color,
+      });
+    }
+  }
+
+  function onMouseDown(e) {
+    drawing = true;
+    current.x = e.clientX;
+    current.y = e.clientY;
+  }
+
+  function onMouseUp(e) {
+    if (!drawing) {
+      return;
+    }
+    drawing = false;
+    drawLine(current.x, current.y, e.clientX, e.clientY, current.color), syncStream;
+  }
+
+  function onMouseMove(e) {
+    if (!drawing) {
+      return;
+    }
+    drawLine(current.x, current.y, e.clientX, e.clientY, current.color, syncStream);
+    current.x = e.clientX;
+    current.y = e.clientY;
+  }
+
+  function onResize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+
+  function throttle(callback, delay) {
+    var previousCall = new Date().getTime();
+    return function () {
+      var time = new Date().getTime();
+
+      if (time - previousCall >= delay) {
+        previousCall = time;
+        callback.apply(null, arguments);
+      }
     };
-    var drawing = false;
-    var syncClient;
-    var $message = $('#message');
-    var syncStream;
+  }
 
-    function drawLine(x0, y0, x1, y1, color) {
-        context.beginPath();
-        context.moveTo(x0, y0);
-        context.lineTo(x1, y1);
-        context.strokeStyle = color;
-        context.lineWidth = 2;
-        context.stroke();
-        context.closePath();
-        if (syncStream) {
-            var w = canvas.width;
-            var h = canvas.height;
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseout", onMouseUp);
+  canvas.addEventListener("mousemove", throttle(onMouseMove, 10));
 
-            syncStream.publishMessage({  // publish the drawing data to Twilio Sync server
-                x0: x0 / w,
-                y0: y0 / h,
-                x1: x1 / w,
-                y1: y1 / h,
-                color: color
-            });
+  var colorBtn = $("#color-btn");
+  var clearBtn = $("#clear-btn");
+
+  function changeColor() {
+    current.color = "#" + Math.floor(Math.random() * 16777215).toString(16); // change line color
+    colorBtn.css("border", "5px solid " + current.color); // change the button border color
+  }
+
+  function clearBoard() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  colorBtn.on("click", changeColor);
+  clearBtn.on("click", clearBoard);
+
+  window.addEventListener("resize", onResize);
+  onResize();
+
+  $.getJSON("/token", function (tokenResponse) {
+
+    syncClient = new Twilio.Sync.Client(tokenResponse.token, { logLevel: "info" });
+    syncClient.on("connectionStateChanged", function (state) {
+        if (state != "connected") {
+          $message.html(
+            'Sync is not live (websocket connection <span style="color: red">' +
+              state +
+              "</span>)..."
+          );
+        } else {
+          $message.html("Sync is live!");
         }
-    }
+      });
 
-    function onMouseDown(e) {
-        drawing = true;
-        current.x = e.clientX;
-        current.y = e.clientY;
-    }
+    syncClient.stream("drawingData").then(function (stream) {
+      syncStream = stream;
+      syncStream.on("messagePublished", function (event) {
+        syncDrawingData(event.message.value);
+      });
 
-    function onMouseUp(e) {
-        if (!drawing) { return; }
-        drawing = false;
-        drawLine(current.x, current.y, e.clientX, e.clientY, current.color, syncStream);
-    }
+      function syncDrawingData(data) {
+        var w = canvas.width;
+        var h = canvas.height;
+        drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+      }
 
-    function onMouseMove(e) {
-        if (!drawing) { return; }
-        drawLine(current.x, current.y, e.clientX, e.clientY, current.color, syncStream);
-        current.x = e.clientX;
-        current.y = e.clientY;
-    }
-
-    function onResize() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    };
-
-    canvas.addEventListener('mousedown', onMouseDown);
-    canvas.addEventListener('mouseup', onMouseUp);
-    canvas.addEventListener('mouseout', onMouseUp);
-    canvas.addEventListener('mousemove', onMouseMove);
-
-    window.addEventListener('resize', onResize);
-    onResize();
-
-    $.getJSON('/token', function(tokenResponse) {
-        syncClient = new Twilio.Sync.Client(tokenResponse.token, { logLevel: 'info' });
-        syncClient.on('connectionStateChanged', function(state) {
-            if (state != 'connected') {
-                $message.html('Sync is not live (websocket connection <span style="color: red">' + state + '</span>)...');
-            } else {
-                $message.html('Sync is live!');
-            }
-            syncClient.stream('drawingData').then(function(stream) {
-            syncStream = stream;
-            syncStream.on('messagePublished', function(event) {
-                syncDrawingData(event.message.value);
-            });
-
-            function syncDrawingData(data) {
-                var w = canvas.width;
-                var h = canvas.height;
-                drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
-            }
-        });
     });
+  });
 });
